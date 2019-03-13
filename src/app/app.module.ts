3e273c7713feccb8ce3562676e02f61dee73974a
122208997 <lehaohust@gmail.com>
2019-03-14 02:26:46 +0700

    Create Basic Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,61 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
 import { OrderHistoryComponent } from './order-history/order-history.component';
 import { CartComponent } from './cart/cart.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ProductOrderComponent } from './product-order/product-order.component';
+
+const routes : Routes = [
+    {
+        path: '',
+        redirectTo: 'products',
+        pathMatch: 'full'
+    },
+    {
+        path: 'products',
+        component: ProductsComponent
+    },
+    {
+        path: 'cart',
+        component: CartComponent
+    },
+    {
+        path: 'order-history',
+        component: OrderHistoryComponent
+    },
+    {
+        path: 'checkout',
+        component: CheckoutComponent
+    },
+    {
+        path: '**',
+        component: NotFoundComponent
+    }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductsComponent,
-    OrderHistoryComponent,
-    CartComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ProductsComponent,
+        OrderHistoryComponent,
+        CartComponent,
+        NotFoundComponent,
+        CheckoutComponent,
+        ProductOrderComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        RouterModule.forRoot(routes)
+    ],
+    providers: [
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-cart',
-  templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+	selector: 'app-cart',
+	templateUrl: './cart.component.html',
+	styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+	constructor(
+		public routerService : Router
+	) { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
+	onBackToProducts() {
+		this.routerService.navigate(['']);
+	}
+
+	onGoToCheckout() {
+		this.routerService.navigateByUrl('checkout');
+	}
 }
+
