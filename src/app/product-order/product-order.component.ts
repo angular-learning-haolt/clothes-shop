ada343b33f112512c388c6314b31a175d6c99814
122208997 <lehaohust@gmail.com>
2019-03-14 23:33:38 +0700

    Limit min Quantity, auto update Price

--- src/app/product-order/product-order.component.ts
+++ src/app/product-order/product-order.component.ts
@@ -8,11 +8,29 @@ import { Product } from './../../app/product';
 })
 export class ProductOrderComponent implements OnInit {
 
-	constructor() { }
+	constructor() {
+	}
 
 	@Input() inCartProducts: any[];
 
+	public summary : number;
+
+
 	// public addedToCartProducts : any[] = this.selectedProducts;
 	ngOnInit() {
+		console.log(this.inCartProducts);
+	}
+
+	changeQuantity(product, val) {
+		switch (val) {
+			case -1:
+				product.quantity = product.quantity !== 0? product.quantity - 1 : 0;
+				break;
+			
+			case 1:
+				product.quantity += 1;
+				break;
+		}
+		localStorage.setItem('inCartProducts', JSON.stringify(this.inCartProducts));
 	}
 }
