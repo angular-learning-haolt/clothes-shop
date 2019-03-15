feacab6e9842c80ce59ff75dc7ea842e08494eb5
122208997 <lehaohust@gmail.com>
2019-03-15 22:19:13 +0700

    Bug: update cart via lifecycle; Not fetch data on hover navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 	title = '=.=';
 
 	constructor(
-	) {}
+	) { }
+
+	public quantityProductInCart : number = 0;
+
+	ngOnInit() {
+	}
+
+	ngAfterContentChecked() {
+		if (localStorage.getItem('inCartProducts') !== null) {
+			if (localStorage.getItem('inCartProducts').split('},{').length > 1) {
+				this.quantityProductInCart = localStorage.getItem('inCartProducts').split('},{').length;
+			}
+			else if (localStorage.getItem('inCartProducts').split('}}').length === 1) {
+				this.quantityProductInCart = 1
+			}
+			else if (localStorage.getItem('inCartProducts') == null) {
+				this.quantityProductInCart = 0
+			}
+		}
+	}
 }
