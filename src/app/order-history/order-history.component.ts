0364f8fc268c9dd4a89e9251e1e1db1787b8bda8
122208997 <lehaohust@gmail.com>
2019-03-15 16:09:05 +0700

    Check isSaveLocalStorage

--- src/app/order-history/order-history.component.ts
+++ src/app/order-history/order-history.component.ts
@@ -2,20 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-order-history',
-  templateUrl: './order-history.component.html',
-  styleUrls: ['./order-history.component.css']
+	selector: 'app-order-history',
+	templateUrl: './order-history.component.html',
+	styleUrls: ['./order-history.component.css']
 })
 export class OrderHistoryComponent implements OnInit {
 
-  constructor(
-  	public routerService : Router
-  ) { }
+	constructor(
+		public routerService : Router
+	) {
+		if (localStorage.getItem('productBills') === null) {
+			localStorage.setItem('productBills', JSON.stringify([]));
+		}
+		else {
+			this.productBills = JSON.parse(localStorage.getItem('productBills'));
+		}
+	}
 
-  ngOnInit() {
-  	console.log(this.productBills);
-  }
+	ngOnInit() {
+		console.log(this.productBills);
+	}
 
-  public productBills : any[] = this.routerService.getCurrentNavigation().extras.state? this.routerService.getCurrentNavigation().extras.state.productBills: null;
+	public productBills : any[] = this.routerService.getCurrentNavigation().extras.state? this.routerService.getCurrentNavigation().extras.state.productBills: null;
 
 }
